050ddc50bcb765018c292405bdea2ffbd7bde5f1
dmliddle <david.liddle@duke.edu>
2024-10-16 22:15:50 -0400

    Task 1 Complete

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,11 @@
 #%% Task 1 - Edit code to print as requested
-/*-PS3: Code Block 1--*/
+#/*-PS3: Code Block 1--*/
 
 mountain = "Denali"
 nickname = 'Mt. McKinley'
-elevation == 20322 
+elevation = 20322 
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+print (mountain + ", sometimes\ncalled " + nickname +", ")
+print ("is " + str(elevation) +"\'above sea level." )
 
 #%% Task 2 - Lists and Iteration
