5798cc5f70a74e9d5eae3575bc7048592043f796
dmliddle <david.liddle@duke.edu>
2024-10-16 22:56:39 -0400

    Task 3 Part I commit

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -22,6 +22,39 @@ user_item = "roads.shp"
 # Step 4: Add the user_item to the data_list
 data_list.append(user_item)
 
-# Step 5: Loop through each item in the data_list and print the full Windows path
+# Step 5: Loop through each item and print full path
 for item in data_list:
     print(data_folder + "\\" + item)
+
+#%% Task 3
+
+# Step 1: Create empty string variable 'user_numbers'
+user_numbers = []
+
+# Step 2: Create for loop to let the user add integers to the string variable
+for x in range(3):
+    y = int(input('Enter an integer: '))
+    user_numbers.append(y)
+
+# Step 3: Sort the user_numbers string variable
+user_numbers.sort()
+
+# Step 4: Print the highest value of user_numbers
+print(user_numbers[-1])
+
+# Task 3 - Challenge
+
+# Step 1: Create empty string variable 'userNumbers'
+userNumbers = []
+
+# Step 2: Create for loop to let the user add integers to the string variable
+for x in range(3):
+    y = int(input('Enter an integer: '))
+    userNumbers.append(y)
+
+# Step 3: Sort the userNumbers string variable
+userNumbers.sort(reverse = True)
+
+# Step 4: Print the highest value of userNumbers
+print(userNumbers)
+
