1aa124b59b0e6bd990c717a17afb73db1cc0ee5d
dmliddle <david.liddle@duke.edu>
2024-10-16 22:23:23 -0400

    Task 2 Completed

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -9,3 +9,19 @@ print (mountain + ", sometimes\ncalled " + nickname +", ")
 print ("is " + str(elevation) +"\'above sea level." )
 
 #%% Task 2 - Lists and Iteration
+
+# Step 1: Assign data_folder to a string
+data_folder = "W:\\859_data\\triangle"
+
+# Step 2: Create a list called data_list with string objects
+data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
+
+# Step 3: Assign user_item to "roads.shp"
+user_item = "roads.shp"
+
+# Step 4: Add the user_item to the data_list
+data_list.append(user_item)
+
+# Step 5: Loop through each item in the data_list and print the full Windows path
+for item in data_list:
+    print(data_folder + "\\" + item)
